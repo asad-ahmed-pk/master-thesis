97e1013ab272618c0bea7b1da0bd8f53217b4323
Asad Ahmed <as.ahmed@jacobs-university.de>
2020-01-23 22:46:34 +0100

    Removed test code from reconstruct visual test file

--- cv_reconstruct/src/test/reconstruct_test.cpp
+++ cv_reconstruct/src/test/reconstruct_test.cpp
@@ -5,6 +5,7 @@
 
 #include "camera/CameraCompute.hpp"
 #include "camera/CameraSettings.hpp"
+#include "camera/CameraCalibParser.hpp"
 #include "reconstruct/Reconstruct3D.hpp"
 
 #include <string>
@@ -18,54 +19,16 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-// constants for camera calibration from dataset
-const Eigen::Matrix3f K1 = (Eigen::Matrix3f() << 837.619011, 0.0, 522.434637,
-                                                0.0, 839.808333, 402.367400,
-                                                0.0, 0.0, 1.0).finished();
-
-const Eigen::Matrix3f K2 = (Eigen::Matrix3f() << 835.542079, 0.0, 511.127987,
-                                                 0.0, 837.180798, 388.337888,
-                                                 0.0, 0.0, 1.0).finished();
-
-const Eigen::Vector2i IMAGE_RESOLUTION {1024, 768 };
-
-const Eigen::Matrix3f R = (Eigen::Matrix3f() << 9.9997625494747e-001, -6.3729476131001e-003, -2.6220373684323e-003,
-                                                6.3750339453031e-003, 9.9997936870410e-001, 7.8810427338438e-004,
-                                                2.6169607251553e-003, -8.0480113703670e-004, 9.9999625189882e-001).finished();
-
-const Eigen::Vector3f T = (Eigen::Vector3f() << 1.194711e-001, 3.144088e-004, 1.423872e-004).finished();
-
 // Test image paths
 const std::string LEFT_IMG_PATH { "test_images/img_00_left.jpg" };
 const std::string RIGHT_IMG_PATH { "test_images/img_00_right.jpg" };
 
 int main(int argc, char** argv)
 {
-    // distortion co-effs
-    Eigen::VectorXf D1(4);
-    D1 << -3.636834e-001, 1.766205e-001, 0.000000e+000, 0.000000e+000;
-
-    Eigen::VectorXf D2(4);
-    D2 << -3.508059e-001, 1.538358e-001, 0.000000e+000, 0.000000e+000;
-
-    // left camera settings
-    Camera::Settings::CameraSettings leftCamSettings;
-    leftCamSettings.ImageResolutionInPixels = IMAGE_RESOLUTION;
-    leftCamSettings.D = D1;
-    leftCamSettings.K = K1;
-
-    // right camera settings
-    Camera::Settings::CameraSettings rightCamSettings;
-    rightCamSettings.ImageResolutionInPixels = IMAGE_RESOLUTION;
-    rightCamSettings.D = D2;
-    rightCamSettings.K = K2;
-
-    // stereo settings
+    // parse calib from file
     Camera::Settings::StereoCameraSettings stereoSetup;
-    stereoSetup.LeftCamSettings = leftCamSettings;
-    stereoSetup.RightCamSettings = rightCamSettings;
-    stereoSetup.R = R;
-    stereoSetup.T = T;
+    Camera::CameraCalibParser parser;
+    parser.ParseStereoCalibJSONFile("../resources/calib/test_calib.json", stereoSetup);
 
     // create camera compute module and get updated stereo setup with rectification applied
     Camera::CameraCompute cameraCompute { stereoSetup };
