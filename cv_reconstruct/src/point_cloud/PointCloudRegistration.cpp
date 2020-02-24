ce0c95559e9421da561fddd096d7c39b0436ae36
Asad Ahmed <asad.ahmed.de@gmail.com>
2020-02-24 14:42:15 +0100

    Fixed debug code placement for point cloud registration

--- cv_reconstruct/src/point_cloud/PointCloudRegistration.cpp
+++ cv_reconstruct/src/point_cloud/PointCloudRegistration.cpp
@@ -71,21 +71,6 @@ namespace PointCloud
             sourceCloud->push_back(pcl::PointXYZ(coordsSource[0], -coordsSource[1], coordsSource[2]));
             targetCloud->push_back(pcl::PointXYZ(coordsTarget[0], -coordsTarget[1], coordsTarget[2]));
 
-            // debugging: save to disk as RGB
-            pcl::PointCloud<pcl::PointXYZRGB> p1; pcl::PointCloud<pcl::PointXYZRGB> p2;
-            pcl::copyPointCloud(*sourceCloud, p1); pcl::copyPointCloud(*targetCloud, p2);
-            for (int i = 0; i < p1.points.size(); i++) {
-                p1.points[i].r = 255;
-                p2.points[i].b = 255;
-            }
-            pcl::io::savePCDFileBinary("keypoints_source.pcd", p1);
-            pcl::io::savePCDFileBinary("keypoints_target.pcd", p2);
-
-            pcl::PointCloud<pcl::PointXYZRGB> both;
-            both += p1;
-            both += p2;
-            pcl::io::savePCDFileBinary("keypoints_both.pcd", both);
-
             // add point cloud correspondences
             correspondences->push_back(pcl::Correspondence(index, index, 1.0));
 
@@ -95,6 +80,23 @@ namespace PointCloud
         // apply world space transforms to both clouds
         pcl::transformPointCloud(*sourceCloud, *sourceCloud, worldTransform);
         pcl::transformPointCloud(*targetCloud, *targetCloud, m_TargetData.WorldTransform);
+
+        // debugging: save to disk as RGB
+#ifndef NDEBUG
+        pcl::PointCloud<pcl::PointXYZRGB> p1; pcl::PointCloud<pcl::PointXYZRGB> p2;
+        pcl::copyPointCloud(*sourceCloud, p1); pcl::copyPointCloud(*targetCloud, p2);
+        for (int i = 0; i < p1.points.size(); i++) {
+            p1.points[i].r = 255;
+            p2.points[i].b = 255;
+        }
+        pcl::io::savePCDFileBinary("keypoints_source.pcd", p1);
+        pcl::io::savePCDFileBinary("keypoints_target.pcd", p2);
+
+        pcl::PointCloud<pcl::PointXYZRGB> both;
+        both += p1;
+        both += p2;
+        pcl::io::savePCDFileBinary("keypoints_both.pcd", both);
+#endif
 
         // compute distances for each correspondence
         int i = 0;
