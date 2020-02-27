b3a6d88ccaa9978bd87d7f56ebd79426fe76aeda
Asad Ahmed <asad.ahmed.de@gmail.com>
2020-02-27 16:41:36 +0100

    Added external GoICP library with modified code

---
+++ external/Go-ICP/include/GoICP/StringTokenizer.hpp
@@ -0,0 +1,32 @@
+
+#ifndef STRINGTOKENIZER_H
+#define STRINGTOKENIZER_H
+
+#include <list>
+#include <string>
+
+namespace GoICP
+{
+    class StringTokenizer
+    {
+
+    private:
+        std::list<std::string> tokens;
+
+    public:
+        StringTokenizer();
+
+        StringTokenizer(std::string str, char delim = ' ');
+
+        StringTokenizer(std::string str, char *delims);
+
+        ~StringTokenizer();
+
+        std::string nextToken();
+
+        bool hasMoreTokens();
+
+        int numberOfTokens();
+    };
+}
+#endif // for STRINGTOKENIZER_H
